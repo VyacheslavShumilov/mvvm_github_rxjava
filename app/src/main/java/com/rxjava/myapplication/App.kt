4077f720a64ad219be541f2b81a25c4acce57b64
user.email <vvshumilov@mail.ru>
2022-10-20 21:15:49 +0300

    added comments App

--- app/src/main/java/com/rxjava/myapplication/App.kt
+++ app/src/main/java/com/rxjava/myapplication/App.kt
@@ -2,7 +2,6 @@ package com.rxjava.myapplication
 
 import android.app.Application
 import android.content.Context
-import androidx.fragment.app.Fragment
 import com.rxjava.myapplication.data.RetrofitUsersRepoImpl
 import com.rxjava.myapplication.domain.repos.UsersRepo
 
@@ -24,5 +23,5 @@ class App : Application() {
 val Context.app: App get() = applicationContext as App
 
 
-//Можно сделать для фрагмента
+//Экстеншн можно сделать для фрагмента
 //val Fragment.app: App get() = requireContext().applicationContext as App
