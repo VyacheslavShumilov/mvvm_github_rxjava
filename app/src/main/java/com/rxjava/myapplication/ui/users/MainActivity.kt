fa952c0c79e775f0cf283baf834dd35dbcdac580
user.email <vvshumilov@mail.ru>
2022-10-20 11:21:53 +0300

    created: App, ProfileActivity MainActivity updated

--- app/src/main/java/com/rxjava/myapplication/ui/users/MainActivity.kt
+++ app/src/main/java/com/rxjava/myapplication/ui/users/MainActivity.kt
@@ -1,12 +1,81 @@
 package com.rxjava.myapplication.ui.users
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rxjava.myapplication.R
+import com.rxjava.myapplication.app
+import com.rxjava.myapplication.databinding.ActivityMainBinding
+import com.rxjava.myapplication.domain.entities.UserEntity
+import com.rxjava.myapplication.ui.profile.ProfileActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = UsersAdapter {
+        viewModel.onUserClick(it)
+    }
+
+    private lateinit var viewModel: UsersContract.ViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+        initViewModel()
     }
+
+    private fun initViewModel() {
+        viewModel = extractViewModel()
+
+        viewModel.progressLiveData.observe(this) { showProgress(it) }
+        viewModel.usersLiveData.observe(this) { showUsers(it) }
+        viewModel.errorLiveData.observe(this) { showError(it) }
+        viewModel.openProfileLiveData.observe(this) { openProfileScreen() }
+    }
+
+    private fun openProfileScreen() {
+        startActivity(Intent(this, ProfileActivity::class.java))
+    }
+
+    private fun extractViewModel(): UsersContract.ViewModel {
+        return lastCustomNonConfigurationInstance as? UsersContract.ViewModel
+            ?: UsersViewModel(app.usersRepo)
+    }
+
+    override fun onRetainCustomNonConfigurationInstance(): UsersContract.ViewModel {
+        return viewModel
+    }
+
+    private fun initViews() {
+        binding.refreshButton.setOnClickListener {
+            viewModel.onRefresh()
+        }
+        initRecyclerView()
+
+        showProgress(false)
+    }
+
+    private fun showUsers(users: List<UserEntity>) {
+        adapter.setData(users)
+    }
+
+    private fun showError(throwable: Throwable) {
+        Toast.makeText(this, throwable.message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showProgress(inProgress: Boolean) {
+        binding.progressBar.isVisible = inProgress
+        binding.usersRecyclerView.isVisible = !inProgress
+    }
+
+    private fun initRecyclerView() {
+        binding.usersRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.usersRecyclerView.adapter = adapter
+    }
+
 }
