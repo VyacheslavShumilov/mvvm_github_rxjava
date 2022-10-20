5f15af097c9277b3c31a5ba8f39061d7db49243d
user.email <vvshumilov@mail.ru>
2022-10-20 11:16:57 +0300

    created: UsersViewModel, SingleEventLiveData

---
+++ app/src/main/java/com/rxjava/myapplication/ui/users/UsersViewModel.kt
@@ -0,0 +1,44 @@
+package com.rxjava.myapplication.ui.users
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.rxjava.myapplication.domain.entities.UserEntity
+import com.rxjava.myapplication.domain.repos.UsersRepo
+import com.rxjava.myapplication.utils.SingleEventLiveData
+
+class UsersViewModel (
+    private val usersRepo: UsersRepo
+) : UsersContract.ViewModel {
+
+    override val usersLiveData: LiveData<List<UserEntity>> = MutableLiveData()
+    override val errorLiveData: LiveData<Throwable> = SingleEventLiveData() // single event
+    override val progressLiveData: LiveData<Boolean> = MutableLiveData()
+    override val openProfileLiveData: LiveData<Unit> = SingleEventLiveData()
+
+    override fun onRefresh() {
+        loadData()
+    }
+
+    override fun onUserClick(userEntity: UserEntity) {
+        openProfileLiveData.mutable().postValue(Unit)
+    }
+
+    private fun loadData() {
+        progressLiveData.mutable().postValue(true)
+        usersRepo.getUsers(
+            onSuccess = {
+                progressLiveData.mutable().postValue(false)
+                usersLiveData.mutable().postValue(it)
+            },
+            onError = {
+                progressLiveData.mutable().postValue(false)
+                errorLiveData.mutable().postValue(it)
+            }
+        )
+    }
+
+    private fun <T> LiveData<T>.mutable(): MutableLiveData<T> {
+        return this as? MutableLiveData<T>
+            ?: throw IllegalStateException("It is not MutableLiveData o_O")
+    }
+}
