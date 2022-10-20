722a5331edf6f80c828676b1be8afa4078de10b9
user.email <vvshumilov@mail.ru>
2022-10-20 11:21:58 +0300

    created: App, ProfileActivity MainActivity updated

---
+++ app/src/main/java/com/rxjava/myapplication/App.kt
@@ -0,0 +1,12 @@
+package com.rxjava.myapplication
+
+import android.app.Application
+import android.content.Context
+import com.rxjava.myapplication.data.retrofit.RetrofitUsersRepoImpl
+import com.rxjava.myapplication.domain.repos.UsersRepo
+
+class App : Application() {
+    val usersRepo: UsersRepo by lazy { RetrofitUsersRepoImpl() }
+}
+
+val Context.app: App get() = applicationContext as App
