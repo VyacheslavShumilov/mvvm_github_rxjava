8ebc837f4554f6cdd63dac3e4c8f3d91d321ab26
user.email <vvshumilov@mail.ru>
2022-10-24 21:55:48 +0300

    added commments

--- app/src/main/java/com/rxjava/myapplication/ui/users/MainActivity.kt
+++ app/src/main/java/com/rxjava/myapplication/ui/users/MainActivity.kt
@@ -10,6 +10,8 @@ import com.rxjava.myapplication.app
 import com.rxjava.myapplication.databinding.ActivityMainBinding
 import com.rxjava.myapplication.domain.entities.UsersEntity
 import com.rxjava.myapplication.ui.profile.ProfileActivity
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,8 +23,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     //вместо Presenter - ViewModel, attach() и detach() не нужны. LiveData умеет отпысываться, т.к. она подключена к жизненному циклу. Без LiveData в MVVM нужно вручную отписыватся
-
     private lateinit var viewModel: UsersContract.ViewModel
+
+    //Чтобы отдельно не заводить переменные для отписки всех ViewModel, можно разово завести одну на всех (для rxjava)
+    private val viewModelDisposable = CompositeDisposable()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,13 +35,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initViews()
-        initViewModel()
-    }
 
-    //изменяются значения ViewModel (interface в UsersContract). Нужно (и можно) только подписаться на ViewModel'и, т.к. тип LiveData
-    private fun initViewModel() {
-        viewModel = extractViewModel()
 
+        // 2022.10.20 работа с LiveData
+        //изменяются значения ViewModel (interface в UsersContract). Нужно (и можно) только подписаться на ViewModel'и, т.к. тип LiveData
         // через observe() подписка на переменные. Переменные измменятся (progressLiveData и т.д.) -> вызовутся функции showProgress/showUsers и т.д.
         // ViewModel вызывает фукнции/сэтить значения в LiveData (а не напрямую в View)
         // технически ссылка у ViewModel на MainActivity есть (this). Некорректно говорить, что ViewModel отвязалась от View (Activity) и больше ничего о ней формально не знает...
@@ -44,12 +46,32 @@ class MainActivity : AppCompatActivity() {
 
         // После поворота экрана подписываюсь на errorLiveData заново, заново создается observer (метод observe() в SingleEventLiveData
 
-        viewModel.progressLiveData.observe(this) { showProgress(it) }
-        viewModel.usersLiveData.observe(this) { showUsers(it) }
-        viewModel.errorLiveData.observe(this) { showError(it) }
-        viewModel.openProfileLiveData.observe(this) { openProfileScreen() } //зажгли событие (переход на новый экран) в UsersViewModel,а в Activity его нужно поймать.
-                                                                                   // Но в названии LiveDat'ы лучше не писать openScreen т.к. как именно будет выглядеть информация Активити не должна знать
+        //viewModel.progressLiveData.observe(this) { showProgress(it) }
+        //viewModel.usersLiveData.observe(this) { showUsers(it) }
+        //viewModel.errorLiveData.observe(this) { showError(it) }
+        //viewModel.openProfileLiveData.observe(this) { openProfileScreen() } //зажгли событие (переход на новый экран) в UsersViewModel,а в Activity его нужно поймать.
+        // Но в названии LiveDat'ы лучше не писать openScreen т.к. как именно будет выглядеть информация Активити не должна знать
+        viewModel = extractViewModel()
+
+        // 2022.10.24 Замена LiveData на Observable. Концептуально - одно и то же,
+        // разница в том, что создаются цепочки сбытий и они хранится в переменной "val viewModelDisposable", теперь можно отписаться в onDestroy() от всех одновременно
+
+        viewModelDisposable.addAll(
+            viewModel.progressLiveData.subscribe{ showProgress(it) },
+                    viewModel.usersLiveData.subscribe{ showUsers(it) }, //каждая сточка - это выражение, которое возвращает значение цепочки
+                    viewModel.errorLiveData.subscribe{ showError(it) },
+                    viewModel.openProfileLiveData.subscribe{ openProfileScreen() }
+        )
+
     }
+
+    // Решается проблема жизненного цикла (например при повороте экрана происходит отписка от ВСЕХ подписок). Но этот подход стараются заменить на Корутины
+    override fun onDestroy() {
+        viewModelDisposable.dispose()
+        super.onDestroy()
+    }
+
+
 
     //правильно Intent создавать  в Activity, а не в ViewModel
     private fun openProfileScreen() {
